13921ddd6e7dee93cdc23a94676f80c082af74fa
dbellol <dbellol@unal.edu.co>
2025-03-03 15:14:49 -0500

    cambio utils

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,34 +1,10 @@
-import { create } from 'zustand'
-import { decodeToken } from '../utils/auth'
-
-interface User {
-  _id: string
-  firstname: string
-  lastname: string
-  email: string
-  mobile: string
-  address: string
-  role: string
-  token: string
+export const decodeToken = (token: string) => {
+  try {
+    const base64Url = token.split('.')[1] // Extraemos la parte del payload
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+    return JSON.parse(atob(base64)) // Decodificamos y parseamos el JSON
+  } catch (error) {
+    console.error('Error decodificando el token:', error)
+    return null
+  }
 }
-
-interface UserStore {
-  user: User | null
-  updateUser: (userData: User) => void
-  clearUser: () => void
-}
-
-// ✅ Inicializa el estado con los datos del token
-const getUserFromToken = (): User | null => {
-  const token = sessionStorage.getItem('token')
-  return token ? decodeToken(token) : null
-}
-
-export const useUserStore = create<UserStore>(set => ({
-  user: getUserFromToken(),
-  updateUser: userData => set({ user: userData }),
-  clearUser: () => {
-    sessionStorage.removeItem('token')
-    set({ user: null })
-  },
-}))
